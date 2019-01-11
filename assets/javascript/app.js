01368b259ddf1599f71df71b38e863f371549fe3
Kevin Patel <kpatel1209@gmail.com>
2019-01-11 00:40:47 -0500

    moment calculations were not working in previous file, so had to completely start over to make better sense of the variables and calculations.  appears to be working fine now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,3 @@
-$(document).ready(function(){
-
 // Initialize Firebase
 let config = {
     apiKey: "AIzaSyBm8IQAYI-s_L8G7jowcwmUKUsSGxhaWH8",
@@ -11,81 +9,82 @@ let config = {
   };
   firebase.initializeApp(config);
 
-  // Variable to reference the database
-  let database = firebase.database();
-  
-  // Click event for adding new train to schedule
-  $("#addTrain").on("click", function(event){
-        event.preventDefault();
+// Database variable
+let database = firebase.database();
 
-        // Receive user input and store the data inside variables
-        let trainName = $("#train-name").val().trim()
-        let destination = $("#destination").val().trim()
-        let firstTrainTime = $("#first-train-time").val().trim()
-        let frequency = $("#frequency").val().trim()  
-                
-        // Object for holding new train data
-        let submitTrain = {
-            trainName: trainName,
-            destination: destination,
-            trainTime: firstTrainTime,
-            frequency: frequency
-        }
+// On click function to submit new train schedule
+$("#submit-train-btn").on("click", function(event){
+    event.preventDefault();
 
-        // Pushes new train data to the database
-        database.ref().push(submitTrain);
+    // Store user input into variables
+    let trainName = $("#train-name").val().trim();
+    let trainDestination = $("#train-destination").val().trim();
+    let trainFirstTime = $("#train-first-time").val().trim();
+    let trainFrequency = $("#train-frequency").val().trim();
 
-        console.log(submitTrain.trainName);
-        console.log(submitTrain.destination);
-        console.log(submitTrain.firstTrainTime);
-        console.log(submitTrain.frequency);
+    // Temporary object for the new train data
+    let newTrainData = {
+        name: trainName,
+        destination: trainDestination,
+        firstTime: trainFirstTime,
+        frequency: trainFrequency
+    };
 
-        // Removes text from text boxes
-        $("#train-name").val("");
-        $("#destination").val("");
-        $("#first-train-time").val("");
-        $("#frequency").val("");
-  });
+    // Push the new tran data into Firebase
+    database.ref().push(newTrainData);
 
-    // Firebase event for adding new train schedules to the database and a row in the html page when a user adds an entry
-    database.ref().on("child_added", function(childSnapshot){
-        let trainData = childSnapshot.val();
-        let trainNames = trainData.trainName;
-        let trainFrequency = trainData.frequency;
-        let trainDestination = trainData.destination;
-        let firstTrain = trainData.trainTime;
+    console.log(newTrainData.name);
+    console.log(newTrainData.destination);
+    console.log(newTrainData.firstTime);
+    console.log(newTrainData.frequency);
 
-        // Calculate the minutes until arrival
-        let remainderTrain = moment().diff(moment.unix(firstTrain), "minutes") %  vtrainFrequency;
-        let minutesTrain = trainFrequency - remainderTrain;
-
-        // Calculate the arrival time, add the minutesTime to the current time
-        let arrivalTrain = moment().add(minutesTrain, "m").format("hh:mm");
-
-        // Add the data for each train into the table on the html page
-        $("#newTrain").append(
-                "<tr><td>" + trainNames + 
-                "</td><td>" + trainDestination + 
-                "</td><td>" + trainFrequency + 
-                "</td><td>" + arrivalTrain + 
-                "</td><td>" + minutesTrain + 
-                "</td></tr>")
+    // Clear the values from the form after submitting new train data
+    $("#train-name").val("");
+    $("#train-destination").val("");
+    $("#train-first-time").val("");
+    $("#train-frequency").val("");
 });
 
+// Function for Firebase to add the new data to the current train schedule table on the html page
+database.ref().on("child_added", function(childSnapshot, prevChildKey){
+    console.log(childSnapshot.val());
 
+    // Variables to store snapshots
+    let trainName = childSnapshot.val().name;
+    let trainDestination = childSnapshot.val().destination;
+    let firstTrainTime = childSnapshot.val().firstTime;
+    let trainFrequency = childSnapshot.val().frequency;
 
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(firstTrainTime);
+    console.log(trainFrequency);
 
+    // Use moment.js to calculate Next Arrival and Minutes Away
+    // The first train time needs to be after the current time
+    let convertFirstTrainTime = moment(firstTrainTime, "hh:mm a").subtract(1, "years");
 
+    // Variable for current time
+    let nowTime = moment().format("hh:mm a");
+    console.log("Current Time: " + nowTime);
 
+    // Variable for the difference between nowTime and firstTrainTime
+    let timeDifference = moment().diff(moment(convertFirstTrainTime), "minutes");
 
+    // Variable for remaining time
+    let timeRemaining = timeDifference % trainFrequency;
 
+    // Variable for minutes until the next train arrival
+    let minutesAway = trainFrequency - timeRemaining;
 
+    // Variable for the next train arrival
+    let nextTrainArrival = moment().add(minutesAway, "minutes").format("hh:mm a");
 
-
-
-
-
-
-
-
+    // This to allow the new train data to appear under the Current Train Schedule table on the html page
+    $("#current-train-schedule > tbody").append("<tr><td>" + 
+        trainName + "</td><td>" + 
+        trainDestination + "</td><td>" + 
+        trainFrequency + "</td><td>" + 
+        nextTrainArrival + "</td><td>" + 
+        minutesAway + "</td></tr>");
 });
